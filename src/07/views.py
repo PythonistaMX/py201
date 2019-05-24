2b04c37dc83e008b10f014b0f98f82d9cd53c3b0
José Luis Chiquete <josech@gmail.com>
2019-05-23 19:59:22 -0500

    190523a

--- src/07/views.py
+++ src/07/views.py
@@ -4,5 +4,4 @@ def index(request):
     return HttpResponse("<h1>Hola, mundo.</h1>")
 
 def vista(request):
-    return HttpResponse('<ul><li>URL: {}</li><li>Método: {}</li>, <li>Codificación; {}</li></ul>'.format(request.url,
-                        request.method, request.encoding))
+    return HttpResponse('<ul><li>URL: {}</li><li>Método: {}</li><li>Codificación: {}</li><li>Argumentos: {}</li></ul>'.format(request.path, request.method, request.encoding, request.GET.dict()))
